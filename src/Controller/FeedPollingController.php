0a1b77fbf774c2366abe6161d89c15983cf9670f
himanshu.mehta09 <himanshumehta.hm9@gmail.com>
2021-09-05 23:57:30 +0530

    removed irregular commenting

--- src/Controller/FeedPollingController.php
+++ src/Controller/FeedPollingController.php
@@ -27,39 +27,13 @@ class FeedPollingController extends AbstractController
     public function urlFeeds(Request $request,$id): Response
     {
 
-	// echo "<pre>";
- //    print_r($id);
- //    die;
         $em = $this->getDoctrine();
         $urlObj = $em->getRepository("App:ManageUrl");
         $feedurls = $urlObj->find($id);
         
         $url = $feedurls->getUrl();
-        // die($url);
-
-
-
-        // $product = $this->getDoctrine()
-        //     ->getRepository(Product::class)
-        //     ->find($id);
-
-
-
-
-		
-		// $feedurls = [
-  //           '1' => 'https://www.axelerant.com/tag/drupal-planet/feed',
-  //           '2' => 'http://feeds.bbci.co.uk/news/technology/rss.xml',
-  //           '3' => 'https://feeds.feedburner.com/symfony/blog',
-            
-  //       ];
-        
-		// $url=$feedurls[$id];
-		
 		$rss = simplexml_load_file($url);
-		// echo "<pre>";
-  //       print_r($rss->channel->item[0]['title']);
-  //       die;
+		
 
         return $this->render('feed_polling/feeds.html.twig', [
             'rss' => $rss,
